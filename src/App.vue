7d40f514ee377d3fe1a3d608c34d9f1903311db2
Dariana Gomez <darigomez30@gmail.com>
2021-04-08 23:12:19 -0400

    subida desafio props

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <label>Tarea</label>
+    <input v-model="tarea" type="text" placeholder="Ingrese nueva tarea">
+    <button @click="ingresarTarea()">Añade</button>
+    <HelloWorld :arreglo="miarreglo"/>
+  </div>
+  
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  
+  name: 'App',
+  data() {
+    return {
+      miarreglo: [],
+      tarea: "",
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    ingresarTarea() {
+      if(!this.tarea) return
+      this.miarreglo.push({tarea: this.tarea})
+      this.tarea = ""
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+label{
+  font-size: 20px;
+  margin: 0 10px;
+  font-weight: bold;
+}
+input{
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+}
+button{
+  width: 6%;
+  padding: 4px;
+  border-radius: 4px;
+  border: 2px solid #6256f0;
+  margin: 0 5px;
+  background-color: #fff;
+  color: #7d96df;
+  border-color:#7d96df;
+;
+  cursor: pointer;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section v-if="arreglo.length === 0">
+      <p>Aquí irán las tareas</p>
+    </section>
+    <section v-else>
+      <ul>
+        <li v-for="(el,index) in arreglo" :key="index">{{el.tarea}}</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    arreglo: {
+      type: Array,
+      default: () => []
+    }
+  },
+}
+</script>
+
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+li {
+  margin: 0 10px;
+  font-size: 20px;
+  font-weight: bold;
+}
+p{
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(33, 128, 145);
+}
+</style>
